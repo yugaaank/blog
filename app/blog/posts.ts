695387447a62e9e2f625d78124f8251ec6295246
yugaaank <yugaankrathore0@gmail.com>
2026-07-15 21:44:57 +0530

    blog added 2026-07-15

--- app/blog/posts.ts
+++ app/blog/posts.ts
@@ -9,6 +9,12 @@ export interface Post {
 export const posts: Post[] = [
   // This week
   {
+    slug: "asm-is-hard",
+    title: "Assembly language is..not for beginners",
+    description: "Why it will take me little more time to learn OS dev",
+    date: "2026-07-15"
+  },
+  {
     slug: "my-first-post",
     title: "My First Post",
     description: "Welcome to my blog, built with Next.js and MDX.",
